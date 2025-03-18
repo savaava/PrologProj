ec44fa1ef9f2a6a08a1041a0bf999a485e8931f2
savaava <andreasavastano4388@gmail.com>
2025-03-18 19:41:55 +0100

    Add family relationship definitions and rules in AlberoGen.pl

---
+++ AlberoGen.pl
@@ -0,0 +1,29 @@
+%%%FATTI%%%
+padre(mario,manuela).
+padre(mario,giulia).
+padre(mario,bruno).
+coniuge(mario,sandra).
+
+padre(salvatore,giovanni).
+coniuge(salvatore,giulia).
+
+padre(brunosenior,mario).
+coniuge(brunosenior,angela).
+
+%%%REGOLE%%%%
+genitore(X,Y) :- 
+    padre(X,Y).
+
+madre(X,Y) :-
+    coniuge(Z,X), padre(Z,Y). % , operatore AND
+
+genitore(X,Y) :-
+    madre(X,Y).
+
+antenato(X,Y) :- genitore(X,Y).
+antenato(X,Y) :- genitore(X,Z), antenato(Z,Y).
+
+fratello_sorella(X,Y) :-
+    padre(Z,X), padre(Z,Y), madre(W,X), madre(W,Y) X\=Y.
+zio_zia(X,Y) :-
+    genitore(Z,Y), fratello_sorella(Z,X).
