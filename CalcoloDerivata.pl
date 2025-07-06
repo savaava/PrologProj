8dafc02b8a3665af46674fbe495ba238bf5d1981
savaava <andreasavastano4388@gmail.com>
2025-07-06 12:00:42 +0200

    Refactor derivative definitions and restore myconstant predicate

--- CalcoloDerivata.pl
+++ CalcoloDerivata.pl
@@ -10,30 +10,23 @@ deriva(log(x),1/x, x).
 deriva(exp(x),exp(x), x).
 
 % f(-x) = -f(-x) per le funzioni considerate
-deriva(sin(-x),-cos(-x),x).
-deriva(cos(-x),sin(-x),x).
-deriva(log(-x), 1/x, x).
+deriva(sin(-x), -cos(-x), x).
+deriva(cos(-x), sin(-x), x).
+deriva(log(-x), -1/x, x).
 deriva(exp(-x), -exp(-x), x).
-
-% definiamo la costante come un valore intero, o reale, o un valore atomico
-myconstant(K) :-
-    integer(K).
-myconstant(K) :-
-    number(K).
-myconstant(K) :-
-    atomic(K).
 
 % Kf(X), dove K è una costante
 deriva(K*F, K*DF, X) :-
     myconstant(K),
-deriva(F, DF, X).
+    deriva(F, DF, X).
 
-deriva(N,0,X) :- 
+deriva(N, 0, _) :- 
+    myconstant(N).
+
+deriva(X^N, N*X^(N-1), X) :-
     integer(N).
-deriva(X^N, N*X^(N-1), X) :- 
-    integer(N). % solo se N è un intero
 
-deriva(X,1,X).
+deriva(X, 1, X).
 
 deriva(F+G, DF + DG, X) :- 
     deriva(F, DF, X),
@@ -46,3 +39,8 @@ deriva(F*G, DF*G + F*DG, X) :-
 deriva(F/G, (DF*G - F*DG)/(G^2), X) :-
     deriva(F, DF, X),
     deriva(G, DG, X).
+
+% definiamo la costante come un valore intero, o reale, o un valore atomico
+myconstant(K) :- integer(K).
+myconstant(K) :- number(K).
+myconstant(K) :- atomic(K).
